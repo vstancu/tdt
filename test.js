c6cd030d7b6a58e5423f62c097dab6912f37fdc0
vvstancu <vvstancu@gmail.com>
2016-11-05 13:01:04 +0200

    test js up

---
+++ test.js
@@ -0,0 +1,25 @@
+re('./server');
+
+describe('server', function () {
+    before(function () {
+        server.listen(1185);
+    });
+
+    after(function () {
+        server.close();
+    });
+});
+
+
+var assert = require('assert'),
+    http = require('http');
+
+describe('/', function () {
+    it('should return 200', function (done) {
+        http.get('http://localhost:1185', function (res) {
+            assert.equal(200, res.statusCode);
+            done();
+        });
+    });
+});
+
