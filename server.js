25a1a51c3303dbb4776daeed98ae5f7aaff6dbd4
vvstancu <vvstancu@gmail.com>
2016-11-05 13:39:04 +0200

    server js modif

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-re('http');
+var http = require('http');
 var fs = require('fs');
 var formidable = require("formidable");
 var util = require('util');
@@ -11,3 +11,37 @@ var server = http.createServer(function (req, res) {
     }
 
 });
+
+function displayForm(res) {
+    fs.readFile('form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields,
+            files: files
+        }));
+    });
+}
+
+
+server.listen(1185);
+console.log("Server running at http://127.0.0.1:1185/");
+
